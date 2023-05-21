b4f442e07d7fc417cd8b1a23bf251225107c8bbe
AlexisOMG <alesha-gramazeka@yandex.ru>
2023-05-21 16:20:55 +0300

    add new tests

--- bk2.py
+++ bk2.py
@@ -1,10 +1,13 @@
 from typing import Callable, Optional, List, Tuple, Dict, Hashable, Any
 from dataclasses import dataclass, field
+from uuid import uuid4
 
 @dataclass
 class Node:
   word: Dict[Hashable, Any]
   children: Dict[int, 'Node'] = field(default_factory=dict)
+  id: str = field(default_factory=lambda: str(uuid4()))
+  used: bool = False
 
   def add_child(self, distance: int, child: 'Node') -> None:
     self.children[distance] = child
@@ -14,10 +17,13 @@ class BKTree:
   def __init__(self, distance_function: Callable[[Dict[Hashable, Any], Dict[Hashable, Any]], int]):
     self.root = None
     self.distance_function = distance_function
+    self.all_nodes: Dict[str, Node] = {}
+    self.cnt = 0
 
   def insert(self, word: Dict[Hashable, Any]) -> None:
     if self.root is None:
       self.root = Node(word)
+      self.all_nodes[self.root.id] = self.root
       return
 
     node = self.root
@@ -28,12 +34,16 @@ class BKTree:
       child = node.children.get(distance)
       if child is None:
         node.add_child(distance, Node(word))
+        self.all_nodes[node.id] = node
+        self.all_nodes[node.children[distance].id] = node.children[distance]
         return
       node = child
 
   def search(self, word: Dict[Hashable, Any], max_distance: int) -> List[Tuple[int, Dict[Hashable, Any]]]:
     if self.root is None:
       return []
+    
+    self.cnt = 0
 
     best_matches = []
     best_distance = max_distance or float('inf')
@@ -42,13 +52,10 @@ class BKTree:
     while nodes_to_visit:
       node, limit = nodes_to_visit.pop()
       distance = self.distance_function(word, node.word)
+      self.cnt += 1
 
-      # if distance < best_distance:
-      #   best_matches.clear()
-      #   best_distance = distance
-      #   best_matches.append((node.word, best_distance))
-      # el
       if distance <= max_distance:
+        node.used = True
         best_matches.append((distance, node.word))
 
       for edge_distance, child in node.children.items():
@@ -56,3 +63,26 @@ class BKTree:
           nodes_to_visit.append((child, max_distance))
 
     return best_matches
+  
+  def find_duplicates(self, max_distance: int) -> List[Tuple[Dict[Hashable, Any], Dict[Hashable, Any]]]:
+    res: List[Tuple[Dict[Hashable, Any], Dict[Hashable, Any]]] = []
+    for id in self.all_nodes:
+      self.all_nodes[id].used = False
+
+    cnt = 0
+
+    for id in self.all_nodes:
+      if self.all_nodes[id].used:
+        continue
+      
+      tmp = self.search(self.all_nodes[id].word, max_distance)
+      cnt += self.cnt
+      # if len(tmp) > 1:
+      for (d, w) in tmp:
+        if d > 0:
+          res.append((w, self.all_nodes[id].word))
+
+    self.cnt = cnt
+
+    return res
+      
